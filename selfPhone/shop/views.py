077b34b05ed78ff740765be711cba5da356dc292
Thomas Tannenberg <Thomas@Tannenberg.online>
2024-04-22 11:29:10 +0200

    fehlerprüfun in register user erstellt

--- selfPhone/shop/views.py
+++ selfPhone/shop/views.py
@@ -8,6 +8,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 # F() Expression ermöglicht es, die Datenbank direkt zu aktualisieren
 from django.db.models import F
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -96,13 +97,15 @@ def logout_user(request):
 
 def register_user(request):
     seite = 'register'
-    user_form = EigeneUserCreationForm()
-    address_form = AddressForm()
     if request.method == 'POST':
         user_form = EigeneUserCreationForm(request.POST)
         address_form = AddressForm(request.POST)
         if user_form.is_valid() and address_form.is_valid():
             benutzer = user_form.save()
+            if Costumer.objects.filter(customer=benutzer).exists():
+                messages.error(request, "Benutzerkonto bereits vorhanden.")
+                return render(request, 'shop/register.html', {'seite': seite,
+                                                              'user_form': user_form, 'address_form': address_form})
 
             customer = Costumer(
                 first_name=user_form.cleaned_data['first_name'],
@@ -136,10 +139,10 @@ def shopBackend(request):
             product = Product.objects.get(id=product_id)
             cart_item, created = CartItem.objects.update_or_create(
                 product=product,
-                # Verwendet F expressions für die sichere Aktualisierung
                 defaults={'quantity': F('quantity') + quantity}
             )
-            return JsonResponse({'message': 'Produkt erfolgreich zum Warenkorb hinzugefügt', 'cart_quantity': cart_item.quantity}, status=200)
+            return JsonResponse({'message': 'Produkt erfolgreich zum Warenkorb hinzugefügt',
+                                 'cart_quantity': cart_item.quantity}, status=200)
         elif action == 'update_cart':
             cart_item_id = data.get('cart_item_id')
             quantity = data.get('quantity')
@@ -150,7 +153,8 @@ def shopBackend(request):
         elif action == 'remove_from_cart':
             cart_item_id = data.get('cart_item_id')
             CartItem.objects.filter(id=cart_item_id).delete()
-            return JsonResponse({'message': 'Artikel erfolgreich aus dem Warenkorb entfernt'}, status=200)
+            return JsonResponse({'message': 'Artikel erfolgreich aus dem Warenkorb entfernt'},
+                                status=200)
         elif action == 'create_order':
             customer = Costumer.objects.get(user=request.user)
             # Hier müssen Sie prüfen, welche Adresse verwendet werden soll
@@ -158,10 +162,14 @@ def shopBackend(request):
             order = Order.objects.create(customer=customer, address=address)
             # Nimmt an, dass CartItem eine Verbindung zu Customer hat
             cart_items = CartItem.objects.filter(
-                product__smartphone__customer=customer)
+                product__smartphone__customer=customer, is_ordered=False)
+            for item in cart_items:
+                item.is_ordered = True
+                item.save()
             order.products.set(cart_items)
             order.save()
-            return JsonResponse({'message': 'Bestellung erfolgreich erstellt', 'order_id': order.id}, status=200)
+            return JsonResponse({'message': 'Bestellung erfolgreich erstellt',
+                                 'order_id': order.id}, status=200)
         else:
             return JsonResponse({'error': 'Unbekannte Aktion'}, status=400)
     except json.JSONDecodeError:
@@ -170,5 +178,11 @@ def shopBackend(request):
         return JsonResponse({'error': 'Produkt nicht gefunden'}, status=404)
     except CartItem.DoesNotExist:
         return JsonResponse({'error': 'Warenkorb-Element nicht gefunden'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Ungültiges JSON'}, status=400)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=404)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
